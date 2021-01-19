59f3d0718ee17aad4e19d2ca08f36af142c3eeb7
Chadaggarwal <chad.aggarwal15@gmail.com>
2021-01-19 13:50:06 -0600

    first commit

---
+++ src/components/App/classList.tsx
@@ -0,0 +1,8 @@
+const classList = {
+    container: "center w-90 mt5 pa4 tc",
+    appTitleBar: 'flex justify-between',
+    appTitle: 'f2',
+    featureButtonWrapper: 'mt3 flex items-center justify-between pa2 bg-light-pink'
+};
+
+export default classList;
---
+++ src/components/Button/classList.tsx
@@ -0,0 +1,8 @@
+
+
+const classList: { [key:string]: string} = {
+    primary: "btn f6 link br1 ba bw ph3 pv2 dib black ba bg-white b--black",
+    resetButton: 'btn f6 link ph3 pv2 bw mb2 dib white bg-dark-green'
+};
+
+export default classList;
